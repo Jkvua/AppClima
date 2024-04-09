0152debdf53caf3455fc05a569b7356772d8b464
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2024-04-09 10:03:55 -0300

    mais dados as funcoes

--- js/index.js
+++ js/index.js
@@ -14,10 +14,12 @@ const key = "98cf6ef447034b90a40122310242203";
 function dadosTela(dados) {
   console.log(dados)
   document.querySelector(".cidade").innerHTML = "Tempo em " + dados.location.name
+  document.querySelector(".pais").innerHTML= dados.location.country
   document.querySelector(".temp").innerHTML = dados.current.temp_c + "°C"
   document.querySelector(".texto-previsao").innerHTML = dados.current.condition.text
   document.querySelector(".umidade").innerHTML = dados.current.humidity + "% de umidade"
   document.querySelector(".img-previsao").src = "https:" + dados.current.condition.icon
+  document.querySelector(".dataHora").innerHTML = dados.location.localtime
 }
 
 async function buscarCidade(cidade) {
