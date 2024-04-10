4c8b86ce5d58b9f375333975cfac1f0e8fac5294
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-04-09 23:01:58 -0300

    tornando a pagina show funcional

--- js/show.js
+++ js/show.js
@@ -1,11 +1,10 @@
 const apiKey = "98cf6ef447034b90a40122310242203";
 
-function dadosShow() {
-    document.querySelector("#")
+function botaoclica() {
+    const input = document.querySelector(".input-cidad").value;
+
+    console.log(input);
 }
 
-async function cidadeEspecifica(){
-    const ddados = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${Rio_de_Janeiro}`)
-    
-    dadosShow(ddados)
-}
+
+//`http://api.weatherapi.com/v1/forecast.json?key=98cf6ef447034b90a40122310242203&q=Brazil&days=7&aqi=yes&alerts=yes`
